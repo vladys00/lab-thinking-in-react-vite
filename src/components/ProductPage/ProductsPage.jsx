2a9575a01add933ef21c5fe89c182f6e111ece57
Vladys K <vldk0022@gmail.com>
2025-01-17 22:45:35 +0100

    done iteration 5

--- src/components/ProductPage/ProductsPage.jsx
+++ src/components/ProductPage/ProductsPage.jsx
@@ -5,11 +5,20 @@ import SearchBar from "../SearchBar/SearchBar";
 
 function ProductsPage() {
   const [products, setProducts] = useState(jsonData);
-
+  const [originalProducts] = useState(jsonData);
+  
+  function searchProduct(value) {
+    const searchTerm = value.toLowerCase();
+    const filteredProducts =  originalProducts.filter(item => 
+      item.name.toLowerCase().startsWith(searchTerm)
+    );
+    setProducts(filteredProducts)
+  }
+  
   return (
     <div>
       <h1>IronStore</h1>
-      <SearchBar/>
+      <SearchBar searchProduct={searchProduct} />
       <ProductTable products={products}/>
     </div>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,18 @@
 import './SearchBar.css'
-function SearchBar() {
+import { useState } from 'react';
+function SearchBar({searchProduct}) {
+    const [iniValue, setValue] = useState("")
+    const updateValue = (event)=> {
+        const newValue = event.target.value
+        setValue(newValue)
+        searchProduct(newValue)
+
+    }
+
+    console.log("Values:",iniValue)
   return (
     <div className="search-container">
-      <input type="text" className="search-bar" placeholder="Search..." />
+      <input  value={iniValue} type="text" className="search-bar" placeholder="Search..." onChange={updateValue} />
       <div className="checkbox-container">
         <input type="checkbox" id="in-stock" />
         <label htmlFor="in-stock">Show only products in stock</label>
